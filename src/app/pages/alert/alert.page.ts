44f5e6c659b7b4f21f6a73c188217d8a3a248b5b
carol cespedes <carolinacespedes90@gmail.com>
2021-01-31 16:11:00 +0100

    add alert button

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from "@ionic/angular";
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.page.html',
@@ -7,9 +7,128 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor( private alertCtrl: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+    async presentAlertMultipleButtons() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      // buttons: ['Cancel', 'Open Modal', 'Delete']
+      buttons: [
+        {
+        text: 'Cancel',
+        role: 'cancel',
+        },
+        {
+          text: 'Open Modal',
+          handler: () => { console.log('modal abierto')}
+        },
+        {
+          text: 'Delete',
+          handler: () => { console.log('borrado')}
+        },
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          id: 'name',
+          placeholder: 'Nombre'
+        },
+        {
+          name: 'lastname',
+          type: 'text',
+          id: 'lastname',
+          placeholder: 'Apellidos'
+        },
+        // multiline input.
+        {
+          name: 'adress',
+          id: 'paragraph',
+          type: 'textarea',
+          placeholder: 'Dirección'
+        },
+        {
+          name: 'url',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Url favorita'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          placeholder: 'dd/mm/aaaa',
+          label: 'Fecha nacimiento',
+          min: '1900-01-01',
+          max: '2021-02-01'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: -5,
+          max: 10
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        },
+        {
+          name: 'name8',
+          type: 'password',
+          placeholder: 'Advanced Attributes',
+          cssClass: 'specialClass',
+          attributes: {
+            maxlength: 4,
+            inputmode: 'decimal'
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data: any) => {
+            console.log('Confirm Ok',data);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
