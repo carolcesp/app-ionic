3f39bb6591e95f970f2633a2cc986614dd19daa3
carol cespedes <carolinacespedes90@gmail.com>
2021-02-07 18:54:24 +0100

    loading

---
+++ src/app/pages/loading/loading.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.page.html',
+  styleUrls: ['./loading.page.scss'],
+})
+export class LoadingPage implements OnInit {
+
+  loading: HTMLIonLoadingElement;
+
+  constructor(public loadingCtrl: LoadingController) { }
+
+  ngOnInit() {
+  }
+
+  showLoading() {
+    this.presentLoading('Cargando ...');
+
+    setTimeout(() => {
+      this.loading.dismiss();
+    }, 1000);
+  }
+
+  async presentLoading(message: string) {
+    this.loading = await this.loadingCtrl.create({
+      message,
+    });
+
+    await this.loading.present();
+  }
+
+}
