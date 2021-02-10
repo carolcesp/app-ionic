a28ac11cb32bc94b6e5e797193a3433b28808b9f
carol cespedes <carolinacespedes90@gmail.com>
2021-02-10 20:57:04 +0100

    example popover

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { HeaderComponent } from './header/header.component';
+import { PopoverInfoComponent } from './popover-info/popover-info.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    PopoverInfoComponent
   ],
   exports: [
     HeaderComponent,
+    PopoverInfoComponent
   ],
   imports: [
     CommonModule,
